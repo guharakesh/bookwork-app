86dcdf8fd1a2940cb732ff9872805f433faee5d0
Rakesh Guha <guha.rakesh@gmail.com>
2013-11-17 20:51:27 +0000

    create profile page for login

--- bookwork/settings.py
+++ bookwork/settings.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 # Load Environment variables from .env file in the Root of the checkout
 
@@ -44,6 +47,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
+    'homepage',
 )
 
 MIDDLEWARE_CLASSES = (
--- bookwork/urls.py
+++ bookwork/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth.views import login, logout
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bookwork.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout),
+    url(r'^accounts/profile/$', include('homepage.urls')),
 )
