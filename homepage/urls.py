3af7d0228bf2fa0b59a5ab7b72ddfa5e45b6d965
Rakesh Guha <guha.rakesh@gmail.com>
2013-11-20 23:11:01 +0000

    add /account/profile url to installed reg app, create home template

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 from homepage import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
+    url(r'^profile/', views.profile, name='profile'),
 )
