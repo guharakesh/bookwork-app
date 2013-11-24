a1b40208e0dfd81794e755cc77c88bd65a7a5555
Sam Roberts <samlroberts+github@gmail.com>
2013-11-24 18:32:20 +0000

    updated login and registraion templates to use our branding

--- bookwork/urls.py
+++ bookwork/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bookwork.views.home', name='home'),
-    url(r'^$', 'splashpage.views.splash', name='splash'),
     # url(r'^blog/', include('blog.urls')),
     # url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
@@ -16,4 +15,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'splashpage.views.splash', name='home')
     # url(r'^homepage/', include('homepage.urls',namespace="homepage"))
     url(r'^accounts/', include('registration.backends.default.urls')),
+    # (r'^login/?$','django.contrib.auth.views.login',{'template_name':'registraion/login.html', 'authentication_form':MyAuthenticationForm}),
 )
