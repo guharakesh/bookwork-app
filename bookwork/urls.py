e3e3500b16205f6f536468c92847ada9a921811f
Rakesh Guha <guha.rakesh@gmail.com>
2013-12-14 01:06:41 +0000

    make emails unique

--- bookwork/urls.py
+++ bookwork/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.views import login, logout
 from django.contrib import admin
+from registration.backends.default.views import RegistrationView
+from registration.backends.default.views import ActivationView
+from registration.forms import RegistrationFormUniqueEmail
 import splashpage
 
 admin.autodiscover()
+
+class RegistrationViewUniqueEmail(RegistrationView):
+    form_class = RegistrationFormUniqueEmail
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,6 +21,7 @@ urlpatterns = patterns('',
     url(r'^splash/', include('splashpage.urls', namespace="splashpage")),
     # url(r'^$', 'splashpage.views.splash', name='home')
     # url(r'^homepage/', include('homepage.urls',namespace="homepage"))
+    url(r'^accounts/register', RegistrationViewUniqueEmail.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls'))
     # (r'^login/?$','django.contrib.auth.views.login',{'template_name':'registraion/login.html', 'authentication_form':MyAuthenticationForm}),
 )
