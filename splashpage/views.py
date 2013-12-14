7442d97462a67677106dd423a2ceb732c3042343
Rakesh Guha <guha.rakesh@gmail.com>
2013-12-14 17:05:22 +0000

    create form on student page

--- splashpage/views.py
+++ splashpage/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from student.models import Student
+from django.forms.models import modelformset_factory
 
 # Create your views here.
 def splash(request):
     # return HttpResponse("Hey you're on the splaspage")
+    StudentFormSet = modelformset_factory(Student)
     if request.user.is_authenticated():
         new_student = Student.objects.get_or_create(user=request.user)[0]
         new_student.save()
-#       User.objects.filter(email=request.user.email)
-#       if not Student.objects.filter(email=request.user.email):
-#           new_student = Student(user=request.user)
-#           new_student.save()
-        return render(request, 'splashpage/base_loggedin.html',{})
+        if request.method == "POST":
+            formset = StudentFormSet(request.POST, request.FILES, queryset=Student.objects.filter(user=request.user))
+            if formset.is_valid():
+                formset.save()
+        else:
+            formset = StudentFormSet(queryset=Student.objects.filter(user=request.user))
+        return render(request, 'splashpage/base_loggedin.html',{"formset": formset})
     else:
         return render(request, 'splashpage/base_splashpage.html',{})
 
