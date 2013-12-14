6f95be2d5b251c19fa70ee9c0726288970d27aef
Rakesh Guha <guha.rakesh@gmail.com>
2013-12-14 14:49:41 +0000

    student appears in admin

--- splashpage/views.py
+++ splashpage/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from student.models import Student
 
 # Create your views here.
 def splash(request):
     # return HttpResponse("Hey you're on the splaspage")
     if request.user.is_authenticated():
-        if not User.objects.filter(email=request.user.email):
-            new_student = Student(user=request.user)
+        new_student = Student.objects.get_or_create(user=request.user)[0]
+        new_student.save()
+#       User.objects.filter(email=request.user.email)
+#       if not Student.objects.filter(email=request.user.email):
+#           new_student = Student(user=request.user)
+#           new_student.save()
         return render(request, 'splashpage/base_loggedin.html',{})
     else:
         return render(request, 'splashpage/base_splashpage.html',{})
