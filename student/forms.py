b264a646e14d65dc201df7c5d40e21340646f84e
Rakesh Guha <guha.rakesh@gmail.com>
2013-12-28 03:24:30 +0000

    attempt chosen widgets

--- student/forms.py
+++ student/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Student, Skill
 from django.contrib.auth.models import User
+from chosen import forms as chosenforms
 
 class UserForm(forms.ModelForm):
     class Meta:
@@ -12,7 +13,5 @@ class StudentForm(forms.ModelForm):
         model = Student
         fields = ['year_in_school','school']
 
-class SkillForm(forms.ModelForm):
-    class Meta:
-        model = Skill
-        fields = ['skill_text']
+class SkillForm(forms.Form):
+    skills = forms.ModelMultipleChoiceField(widget=chosenforms.ChosenSelectMultiple, queryset=Skill.objects.all())
