7ed11fd0304addd14e013bf7b0bb3a8607404361
Rakesh Guha <guha.rakesh@gmail.com>
2014-01-21 21:54:07 +0000

    make names required

--- bookwork/urls.py
+++ bookwork/urls.py
@@ -34,6 +34,8 @@ class RegistrationFormUniqueEmailNoUsername(RegistrationFormUniqueEmail):
         self.fields['username'].initial = id_generator()
         self.fields.insert(0,'last_name',forms.CharField())
         self.fields.insert(0,'first_name',forms.CharField())
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
 
 class RegistrationViewUniqueEmailNoUsername(RegistrationView):
     form_class = RegistrationFormUniqueEmailNoUsername
