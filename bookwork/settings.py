341e0e2cb4ffe5808a7fa7186cad2307df37eb10
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-01-24 14:29:16 -0500

    lol, use staticfiles

--- bookwork/settings.py
+++ bookwork/settings.py
@@ -132,7 +132,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'bookwork/static'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
