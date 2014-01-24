323c4063595f4409a38c021985f094dfab3164f4
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-01-24 14:09:08 -0500

    make dj-static work locally for serving CSS/JS

--- bookwork/settings.py
+++ bookwork/settings.py
@@ -129,7 +129,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'bookwork/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
