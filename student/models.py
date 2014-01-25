839ad75ae4ffc428788359c25b8dfacaffc71c5c
Rakesh Guha <guha.rakesh@gmail.com>
2014-01-25 01:24:46 +0000

    import boolean field, migrate m2m

--- student/models.py
+++ student/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django.db.models import BooleanField
 
 # Create your models here.
 
