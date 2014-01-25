cf9b399e1e82a917da46639fe0f034fb58982275
Rakesh Guha <guha.rakesh@gmail.com>
2014-01-25 00:56:24 +0000

    remove select2 imports

--- student/models.py
+++ student/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
-import select2.fields
-import select2.models
 
 # Create your models here.
 
