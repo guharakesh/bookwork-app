238a5a1eae5b4054402550843777066fd5a6d092
Rakesh Guha <guha.rakesh@gmail.com>
2014-05-14 21:00:52 +0000

    change school field choices

--- student/models.py
+++ student/models.py
@@ -112,7 +112,7 @@ class Student(models.Model):
         ('YALE','Yale University'),
         ('OTHR','Other'))
     
-    school = models.CharField(max_length=50, choices=SCHOOL_CHOICES, default='CWRU')
+    school = models.ForeignKey(School)
 
     def getPictureURL(self):
         try:
