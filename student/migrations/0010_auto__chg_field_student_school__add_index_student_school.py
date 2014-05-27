24104d88627ce6d7a8788e9533afb0db2fbd745f
Rakesh Guha <guha.rakesh@gmail.com>
2014-05-27 15:33:32 +0000

    change model, add migration

--- student/migrations/0010_auto__chg_field_student_school__add_index_student_school.py
+++ student/migrations/0010_auto__chg_field_student_school__add_index_student_school.py
@@ -1,124 +1,31 @@
 # -*- coding: utf-8 -*-
 from south.utils import datetime_utils as datetime
 from south.db import db
-from south.v2 import DataMigration
+from south.v2 import SchemaMigration
 from django.db import models
-import codecs
 
-class Migration(DataMigration):
+
+class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName". 
-        # Use orm.ModelName to refer to models in this application,
-        # and orm['appname.ModelName'] for models in other applications.
 
-        with codecs.open('/home/bookwork/app/scripts/schools/out.txt', encoding='utf-8', mode='r') as f:
-            data = f.readlines() 
-            for line in data:
-                values = line.split('|')
-                sql_str = "INSERT INTO student_school VALUES (%s, '%s');" % (values[0],values[1])
-
-                db.execute(sql_str)
-
-        db.add_column('student_student', 'school_id', models.IntegerField(null=True))
-
-        for stud in orm['student.Student'].objects.all():
-            if stud.school == 'BOSC':
-                stud.school_id = 1180
-            if stud.school == 'BRAN':
-                stud.school_id = 1188 
-            if stud.school == 'BWRN':
-                stud.school_id = 1203 
-            if stud.school == 'CALT':
-                stud.school_id = 1221
-            if stud.school == 'CMU_':
-                stud.school_id = 1262 
-            if stud.school == 'CWRU':
-                stud.school_id = 1267
-            if stud.school == 'CW_M':
-                stud.school_id = 1387
-            if stud.school == 'COLU':
-                stud.school_id = 1403
-            if stud.school == 'CORN':
-                stud.school_id = 1430
-            if stud.school == 'DART':
-                stud.school_id = 123 
-            if stud.school == 'DUKE':
-                stud.school_id = 169
-            if stud.school == 'EMRY':
-                stud.school_id = 215
-            if stud.school == 'GRGE': 
-                stud.school_id = 288
-            if stud.school == 'GTCH': 
-                stud.school_id = 291
-            if stud.school == 'HRVD': 
-                stud.school_id = 1462
-            if stud.school == 'JHU_': 
-                stud.school_id = 384
-            if stud.school == 'LHGH': 
-                stud.school_id = 424
-            if stud.school == 'MIT_': 
-                stud.school_id = 1583
-            if stud.school == 'NYU_': 
-                stud.school_id = 527 
-            if stud.school == 'NRTH': 
-                stud.school_id = 570
-            if stud.school == 'PRIN': 
-                stud.school_id = 1787
-            if stud.school == 'RICE': 
-                stud.school_id = 651
-            if stud.school == 'STAN': 
-                stud.school_id = 1997
-            if stud.school == 'TUFT': 
-                stud.school_id = 749
-            if stud.school == 'UCLA': 
-                stud.school_id = 823
-            if stud.school == 'UCSD': 
-                stud.school_id = 826
-            if stud.school == 'UCSF': 
-                stud.school_id = 827
-            if stud.school == 'UCHI': 
-                stud.school_id = 836
-            if stud.school == 'MICH': 
-                stud.school_id = 906
-            if stud.school == 'UNC_': 
-                stud.school_id = 941
-            if stud.school == 'NRTD': 
-                stud.school_id = 955
-            if stud.school == 'PENN': 
-                stud.school_id = 2047
-            if stud.school == 'URCH': 
-                stud.school_id = 2073
-            if stud.school == 'USC_': 
-                stud.school_id = 2084
-            if stud.school == 'UVA_': 
-                stud.school_id = 2132
-            if stud.school == 'WISC': 
-                stud.school_id = 2145
-            if stud.school == 'VAND': 
-                stud.school_id = 966
-            if stud.school == 'WAKE': 
-                stud.school_id = 990
-            if stud.school == 'WUSL': 
-                stud.school_id = 1012
-            if stud.school == 'YALE': 
-                stud.school_id = 2159
-            if stud.school == 'OTHR':
-                stud.school_id = 2165
-            stud.save()
-
-        db.delete_column('student_student', 'school')
-        db.rename_column('student_student', 'school_id', 'school')
-
+        # Renaming column for 'Student.school' to match new field type.
+        db.rename_column(u'student_student', 'school', 'school_id')
         # Changing field 'Student.school'
-        #db.alter_column(u'student_student', 'school_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['student.School']))
-        #db.execute("ALTER TABLE student_student ALTER COLUMN school_id TYPE integer USING (school_id::integer);")
+        db.alter_column(u'student_student', 'school_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['student.School']))
         # Adding index on 'Student', fields ['school']
         db.create_index(u'student_student', ['school_id'])
 
+
     def backwards(self, orm):
-        "Write your backwards methods here."
+        # Removing index on 'Student', fields ['school']
+        db.delete_index(u'student_student', ['school_id'])
+
+
+        # Renaming column for 'Student.school' to match new field type.
+        db.rename_column(u'student_student', 'school_id', 'school')
+        # Changing field 'Student.school'
+        db.alter_column(u'student_student', 'school', self.gf('django.db.models.fields.CharField')(max_length=50))
 
     models = {
         u'auth.group': {
@@ -183,7 +90,7 @@ class Migration(DataMigration):
             'Meta': {'object_name': 'Student'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'profile_pic': ('django.db.models.fields.files.ImageField', [], {'default': "'pic_folder/None/no-img.jpg'", 'max_length': '100'}),
-            'school': ('django.db.models.fields.CharField', [], {'default': "'CWRU'", 'max_length': '50'}),
+            'school': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['student.School']"}),
             'skills': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['student.Skill']", 'symmetrical': 'False'}),
             'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
             'year_in_school': ('django.db.models.fields.CharField', [], {'default': "'FR'", 'max_length': '2'})
@@ -191,4 +98,3 @@ class Migration(DataMigration):
     }
 
     complete_apps = ['student']
-    symmetrical = True
