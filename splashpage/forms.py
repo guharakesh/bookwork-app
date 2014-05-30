ee16ab36af04f6a697270e42f6c24b8484a74d46
Sam Roberts <samlroberts+github@gmail.com>
2014-05-30 02:12:24 +0000

    fix student or employer page and some template changes

--- splashpage/forms.py
+++ splashpage/forms.py
@@ -8,3 +8,7 @@ class TypeForm(forms.Form):
 
     user_type = forms.ChoiceField(choices=CHOICES, required=True)
 
+    def __init__(self, *args, **kwargs):
+        super(TypeForm, self).__init__(*args, **kwargs)
+
+        self.fields['user_type'].label = "I am a"
